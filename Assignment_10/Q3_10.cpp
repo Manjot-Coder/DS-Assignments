4448dd8091ec8b866a0439b46b039172176bd1d6
Manjot-Coder <manjot2206@gmail.com>
2025-11-28 16:05:44 +0530

    Delete Assignment_10 directory

--- Assignment_10/Q3_10.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-    vector<int> nums={2,3,2,4,3,2};
-    unordered_map<int,int> f;
-    for(int x:nums) f[x]++;
-    for(auto &p:f) cout<<p.first<<" -> "<<p.second<<" times\n";
-}
